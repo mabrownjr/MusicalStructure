c0b1fa4b9006c76d6f87d992f17d7ce96f2cc0af
Michael <mikeabrownjr@gmail.com>
2017-05-29 23:04:15 -0400

    v1 of Musical Structure

---
+++ LyricsListActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.musicalstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static com.example.android.musicalstructure.R.layout.activity_lyricslist;
+
+public class LyricsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_lyricslist);
+    }
+}
